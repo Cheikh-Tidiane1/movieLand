4d9e61a29c8a729b35fc1f7d73e9d5a1b07e3f4a
cheikh tidiane <cheikh951812@gmail.com>
2024-04-02 23:27:26 +0200

    Models: Movie

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Movie } from '../../models/movies';
 
 @Component({
   selector: 'slider',
   standalone: true,
   imports: [],
   templateUrl: './slider.component.html',
-  styleUrl: './slider.component.scss'
+  styleUrl: './slider.component.scss',
 })
-export class SliderComponent {
-
+export class SliderComponent implements OnInit {
+  @Input() items: Movie;
+  constructor() {}
+  ngOnInit(): void {}
 }

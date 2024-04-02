523062fa18f504c49316e93fc18183fd20f2f74b
cheikh tidiane <cheikh951812@gmail.com>
2024-04-03 00:06:16 +0200

    Slider Component: update

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Movie } from '../../models/movies';
-
+import { DatePipe } from '@angular/common';
 @Component({
   selector: 'slider',
   standalone: true,
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './slider.component.html',
   styleUrl: './slider.component.scss',
 })
 export class SliderComponent implements OnInit {
-  @Input() items: Movie;
+  @Input() items: Movie[];
   constructor() {}
   ngOnInit(): void {}
 }

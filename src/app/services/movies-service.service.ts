7e5fc0abb0b792392da71ca7eb3cfd5bccb6bb4a
cheikh tidiane <cheikh951812@gmail.com>
2024-04-04 21:49:29 +0200

    provideAnimations

--- src/app/services/movies-service.service.ts
+++ src/app/services/movies-service.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MoviesServiceService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-  getMovies(){
-    return this.http.get('https://api.themoviedb.org/3/movie/upcoming?api_key=1a484aa47d89f6331c855cf5d0229ca5')
+  getMovies() {
+    return this.http.get(
+      'https://api.themoviedb.org/3/movie/upcoming?api_key=1a484aa47d89f6331c855cf5d0229ca5'
+    );
   }
 }

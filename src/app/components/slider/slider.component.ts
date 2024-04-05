af302047ea94eec6ffe113dde7c8a77616065ae5
cheikh tidiane <cheikh951812@gmail.com>
2024-04-05 23:39:46 +0200

    sliderAnimations

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,12 +1,19 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Movie } from '../../models/movies';
 import { DatePipe } from '@angular/common';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 @Component({
   selector: 'slider',
   standalone: true,
   imports: [DatePipe],
   templateUrl: './slider.component.html',
   styleUrl: './slider.component.scss',
+  animations: [
+    trigger('slideFade' , [
+      state('void', style({opacity: 0})),
+      transition('void <=> *', [animate('1s')]),
+    ])
+  ]
 })
 export class SliderComponent implements OnInit {
   @Input() items: Movie[];

e145165a40157756c5226712de2d21ae2dfae925
cheikh tidiane <cheikh951812@gmail.com>
2024-04-08 13:14:25 +0200

    feat: movie item style

--- src/app/components/items-banner/items-banner.component.ts
+++ src/app/components/items-banner/items-banner.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { MovieItemComponent } from "../movie-item/movie-item.component";
 
 @Component({
-  selector: 'items-banner',
-  standalone: true,
-  imports: [],
-  templateUrl: './items-banner.component.html',
-  styleUrl: './items-banner.component.scss',
+    selector: 'items-banner',
+    standalone: true,
+    templateUrl: './items-banner.component.html',
+    styleUrl: './items-banner.component.scss',
+    imports: [MovieItemComponent]
 })
 export class ItemsBannerComponent implements OnInit {
   constructor() {}
---
+++ src/app/components/movie-item/movie-item.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'movie-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './movie-item.component.html',
+  styleUrl: './movie-item.component.scss'
+})
+export class MovieItemComponent implements OnInit {
+  ngOnInit(): void {
+  }
+
+}

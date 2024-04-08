7c1dfc844e2d7c2030c2d3766800162cafa7a656
cheikh tidiane <cheikh951812@gmail.com>
2024-04-08 14:50:10 +0200

    refactor(component): movie-item to item

---
+++ src/app/components/item/item.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'item',
+  standalone: true,
+  imports: [],
+  templateUrl: './item.component.html',
+  styleUrl: './item.component.scss'
+})
+export class ItemComponent implements OnInit {
+  ngOnInit(): void {
+  }
+
+}
--- src/app/components/items-banner/items-banner.component.ts
+++ src/app/components/items-banner/items-banner.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { MovieItemComponent } from "../movie-item/movie-item.component";
+import { ItemComponent } from '../item/item.component';
 
 @Component({
-    selector: 'items-banner',
-    standalone: true,
-    templateUrl: './items-banner.component.html',
-    styleUrl: './items-banner.component.scss',
-    imports: [MovieItemComponent]
+  selector: 'items-banner',
+  standalone: true,
+  templateUrl: './items-banner.component.html',
+  styleUrl: './items-banner.component.scss',
+  imports: [ItemComponent],
 })
 export class ItemsBannerComponent implements OnInit {
   constructor() {}

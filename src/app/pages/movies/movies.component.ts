b6cbb0948a0f231bb5c4c65d78ba594a6340de83
cheikh tidiane <cheikh951812@gmail.com>
2024-04-09 16:44:49 +0200

    feat: getPageMovies

--- src/app/pages/movies/movies.component.ts
+++ src/app/pages/movies/movies.component.ts
@@ -2,20 +2,27 @@ import { Component, OnInit } from '@angular/core';
 import { Movie } from '../../models/movies';
 import { MoviesServiceService } from '../../services/movies-service.service';
 import { ItemComponent } from "../../components/item/item.component";
-
+import { PaginatorModule } from 'primeng/paginator';
 @Component({
     selector: 'app-movies',
     standalone: true,
     templateUrl: './movies.component.html',
     styleUrl: './movies.component.scss',
-    imports: [ItemComponent]
+    imports: [ItemComponent,PaginatorModule]
 })
 export class MoviesComponent implements OnInit {
   movies: Movie[] = [];
   constructor(private movieService: MoviesServiceService) {}
   ngOnInit(): void {
+    this.getPageMovies(1)
+  }
+
+  getPageMovies(page: number){
     this.movieService
-      .searchMovies(1)
-      .subscribe((movies) => (this.movies = movies));
+    .searchMovies(page)
+    .subscribe((movies) => (this.movies = movies));
+  }
+  paginate(event: any){
+    this.getPageMovies(event.page + 1)
   }
 }

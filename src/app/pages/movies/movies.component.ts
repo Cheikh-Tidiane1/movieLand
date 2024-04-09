ef062cae31662d5607921f1274c77618bcdfba9f
cheikh tidiane <cheikh951812@gmail.com>
2024-04-09 14:28:44 +0200

    feat: Movies page layput

--- src/app/pages/movies/movies.component.ts
+++ src/app/pages/movies/movies.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Movie } from '../../models/movies';
+import { MoviesServiceService } from '../../services/movies-service.service';
+import { ItemComponent } from "../../components/item/item.component";
 
 @Component({
-  selector: 'app-movies',
-  standalone: true,
-  imports: [],
-  templateUrl: './movies.component.html',
-  styleUrl: './movies.component.scss'
+    selector: 'app-movies',
+    standalone: true,
+    templateUrl: './movies.component.html',
+    styleUrl: './movies.component.scss',
+    imports: [ItemComponent]
 })
-export class MoviesComponent {
-
+export class MoviesComponent implements OnInit {
+  movies: Movie[] = [];
+  constructor(private movieService: MoviesServiceService) {}
+  ngOnInit(): void {
+    this.movieService
+      .getMovies('popular', 20)
+      .subscribe((movies) => (this.movies = movies));
+  }
 }

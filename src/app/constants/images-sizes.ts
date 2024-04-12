8cd126d139e7037b130e0a1338f9f1746192f6d4
cheikh tidiane <cheikh951812@gmail.com>
2024-04-13 01:24:18 +0200

    feat: movie overview

--- src/app/constants/images-sizes.ts
+++ src/app/constants/images-sizes.ts
@@ -2,6 +2,7 @@ const imageBaseUrl = 'https://image.tmdb.org/t/p';
 
 export const IMAGES_SIZES = {
   small: `${imageBaseUrl}/w185`,
+  mid: `${imageBaseUrl}/w300`,
   medium: `${imageBaseUrl}/w780`,
   large: `${imageBaseUrl}/original`
 };
--- src/app/pages/movie/movie.component.ts
+++ src/app/pages/movie/movie.component.ts
@@ -2,22 +2,23 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Movie } from '../../models/movies';
 import { MoviesServiceService } from '../../services/movies-service.service';
-import { JsonPipe } from '@angular/common';
+import { DatePipe, UpperCasePipe, CurrencyPipe } from '@angular/common';
 import { SliderComponent } from "../../components/slider/slider.component";
 import { TabViewModule } from 'primeng/tabview';
+import { IMAGES_SIZES } from '../../constants/images-sizes';
+
 @Component({
     selector: 'app-movie',
     standalone: true,
     templateUrl: './movie.component.html',
     styleUrl: './movie.component.scss',
-    imports: [JsonPipe, SliderComponent,TabViewModule]
+    imports: [DatePipe, SliderComponent,TabViewModule,UpperCasePipe,CurrencyPipe]
 })
 export class MovieComponent implements OnInit {
   movie: Movie;
-  constructor(
-    private route: ActivatedRoute,
-    private movieService: MoviesServiceService
-  ) {}
+  imazeSizes = IMAGES_SIZES;
+
+  constructor(private route: ActivatedRoute,private movieService: MoviesServiceService) {}
   ngOnInit(): void {
     this.route.params.subscribe(({ id }) => {
       this.getMovie(id);

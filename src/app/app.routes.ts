9dfd9da6532120136c0394d571af687cc1679358
cheikh tidiane <cheikh951812@gmail.com>
2024-04-22 17:24:58 +0200

    feat(genres_Component): Movies genres

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { MoviesComponent } from './pages/movies/movies.component';
 import { MovieComponent } from './pages/movie/movie.component';
+import { GenresComponent } from './pages/genres/genres.component';
 
 export const routes: Routes = [
     {path: '' ,component: HomeComponent , title: "Home"},
     {path: 'movies' ,component: MoviesComponent , title: "Movies"},
     {path: 'movie/:id' ,component: MovieComponent , title: "Movie"},
+    {path: 'genres' ,component: GenresComponent , title: "Genres"},
     {path: '**' ,redirectTo: ''},
 
 ];

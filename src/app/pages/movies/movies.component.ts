219f7ded1c4decaaa9489f41420afc8b3ad49abb
cheikh tidiane <cheikh951812@gmail.com>
2024-04-22 19:15:35 +0200

    feat: display movies by genre

--- src/app/pages/movies/movies.component.ts
+++ src/app/pages/movies/movies.component.ts
@@ -1,28 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 import { Movie } from '../../models/movies';
 import { MoviesServiceService } from '../../services/movies-service.service';
-import { ItemComponent } from "../../components/item/item.component";
+import { ItemComponent } from '../../components/item/item.component';
 import { PaginatorModule } from 'primeng/paginator';
+import { ActivatedRoute } from '@angular/router';
+import { take } from 'rxjs';
 @Component({
-    selector: 'app-movies',
-    standalone: true,
-    templateUrl: './movies.component.html',
-    styleUrl: './movies.component.scss',
-    imports: [ItemComponent,PaginatorModule]
+  selector: 'app-movies',
+  standalone: true,
+  templateUrl: './movies.component.html',
+  styleUrl: './movies.component.scss',
+  imports: [ItemComponent, PaginatorModule],
 })
 export class MoviesComponent implements OnInit {
   movies: Movie[] = [];
-  constructor(private movieService: MoviesServiceService) {}
+  constructor(
+    private movieService: MoviesServiceService,
+    private router: ActivatedRoute
+  ) {}
   ngOnInit(): void {
-    this.getPageMovies(1)
+    this.router.params.pipe(take(1)).subscribe(({ genresId }) => {
+      if (genresId) {
+        this.getMoviesByGenres(genresId);
+      } else {
+        this.getPageMovies(1);
+      }
+    });
   }
 
-  getPageMovies(page: number){
+  getPageMovies(page: number) {
     this.movieService
-    .searchMovies(page)
-    .subscribe((movies) => (this.movies = movies));
+      .searchMovies(page)
+      .subscribe((movies) => (this.movies = movies));
   }
-  paginate(event: any){
-    this.getPageMovies(event.page + 1)
+
+  getMoviesByGenres(genresId: string) {
+    this.movieService
+      .getMoviesByGenre(genresId)
+      .subscribe((movies) => (this.movies = movies));
+  }
+
+  paginate(event: any) {
+    this.getPageMovies(event.page + 1);
   }
 }

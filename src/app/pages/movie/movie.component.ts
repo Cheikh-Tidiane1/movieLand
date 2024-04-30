71032c5bbedeb393d090ddf6ea0b6f615b69aa67
cheikh tidiane <cheikh951812@gmail.com>
2024-04-30 15:21:53 +0200

    feat: items banner for similamovies

--- src/app/pages/movie/movie.component.ts
+++ src/app/pages/movie/movie.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Movie, MovieCredits, MovieImages, MovieVideo } from '../../models/movies';
+import {
+  Movie,
+  MovieCredits,
+  MovieImages,
+  MovieVideo,
+} from '../../models/movies';
 import { MoviesServiceService } from '../../services/movies-service.service';
 import {
   JsonPipe,
@@ -16,40 +21,42 @@ import { VideoEmbedComponent } from '../../components/video-embed/video-embed.co
 import { DurationPipe } from '../../pipes/duration.pipe';
 import { ImageModule } from 'primeng/image';
 import { CarouselModule } from 'primeng/carousel';
+import { ItemsBannerComponent } from "../../components/items-banner/items-banner.component";
 
 @Component({
-  selector: 'app-movie',
-  standalone: true,
-  templateUrl: './movie.component.html',
-  styleUrl: './movie.component.scss',
-  imports: [
-    DatePipe,
-    SliderComponent,
-    TabViewModule,
-    UpperCasePipe,
-    CurrencyPipe,
-    JsonPipe,
-    VideoEmbedComponent,
-    CommonModule,
-    DurationPipe,
-    ImageModule,
-    CarouselModule
-  ],
+    selector: 'app-movie',
+    standalone: true,
+    templateUrl: './movie.component.html',
+    styleUrl: './movie.component.scss',
+    imports: [
+        DatePipe,
+        SliderComponent,
+        TabViewModule,
+        UpperCasePipe,
+        CurrencyPipe,
+        JsonPipe,
+        VideoEmbedComponent,
+        CommonModule,
+        DurationPipe,
+        ImageModule,
+        CarouselModule,
+        ItemsBannerComponent
+    ]
 })
-
 export class MovieComponent implements OnInit, OnDestroy {
   movie: Movie;
   imazeSizes = IMAGES_SIZES;
   movieVideos: MovieVideo[] = [];
   moviesImages: MovieImages;
   movieCredits: MovieCredits;
+  similarMovies: Movie[] = [];
   constructor(
     private route: ActivatedRoute,
     private movieService: MoviesServiceService
   ) {}
 
   ngOnDestroy(): void {
-    console.log("component destroyed");
+    console.log('component destroyed');
   }
 
   ngOnInit(): void {
@@ -58,6 +65,7 @@ export class MovieComponent implements OnInit, OnDestroy {
       this.getMovieVideos(id);
       this.getMovieImages(id);
       this.getMovieCredits(id);
+      this.getSimilarMovies(id);
     });
   }
 
@@ -74,15 +82,21 @@ export class MovieComponent implements OnInit, OnDestroy {
     });
   }
 
-  getMovieImages(id: string){
+  getMovieImages(id: string) {
     this.movieService.getMovieImages(id).subscribe((movieImagesData) => {
       this.moviesImages = movieImagesData;
-    })
+    });
   }
 
   getMovieCredits(id: string) {
     this.movieService.getMovieCredits(id).subscribe((movieCreditsData) => {
       this.movieCredits = movieCreditsData;
+    });
+  }
+
+  getSimilarMovies(id: string) {
+    this.movieService.getMovieSimilar(id).subscribe((movieSimilarData) => {
+      this.similarMovies = movieSimilarData;
     });
   }
 }
